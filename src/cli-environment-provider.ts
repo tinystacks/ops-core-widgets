8cd027b5efdc86217fefc211e1e6852c8575ee9c
zsimjee <zaydsimjee@gmail.com>
2023-03-16 12:20:16 -0700

    fix provider export

--- src/cli-environment-provider.ts
+++ src/cli-environment-provider.ts
@@ -1,7 +1,5 @@
 import { Provider } from '@tinystacks/ops-model';
 
-interface CliEnvironmentProvider extends Provider {
+export interface CliEnvironmentProvider extends Provider {
   getCliEnvironment(...args: any): {[ key: string]: string } | Promise<{[ key: string]: string }>;
 }
-
-export default CliEnvironmentProvider;
